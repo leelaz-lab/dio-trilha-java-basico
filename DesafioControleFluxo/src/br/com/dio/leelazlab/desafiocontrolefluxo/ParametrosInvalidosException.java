b00885044f6ddc857231f7778c9dc4b55195c618
leela-lab <georgia.zukas@gmail.com>
2025-08-02 16:09:43 -0300

    Corrigindo estrutura da pasta DesafioControleFluxo e adicionando arquivos

---
+++ DesafioControleFluxo/src/br/com/dio/leelazlab/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package br.com.dio.leelazlab.desafiocontrolefluxo;
+
+public class ParametrosInvalidosException extends RuntimeException{
+	
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+
+}
